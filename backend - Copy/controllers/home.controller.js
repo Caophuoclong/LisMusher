7a56f3f2e457c719cf81322d2af5051c1ff8c4c4
phuoclong <caophuoclong1@gmail.com>
2021-08-12 14:35:53 +0700

    delete

--- backend - Copy/controllers/home.controller.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  getHome: (req, res) => {
-    res.status(200).send({ xinchao: "Chao ban" });
-  },
-  postHome: (req, res) => {
-    console.log(req);
-  },
-};
--- backend - Copy/routes/dashboard.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const dashboardController = require("../controllers/dashboard.controller");
-
-router.get("/:id", dashboardController.response);
-// router.post("/", dashboardController.postHome);
-
-module.exports = router;
